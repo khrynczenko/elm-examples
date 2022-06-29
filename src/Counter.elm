f01058eb102013b748e077e7a8b9b6f305452050
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-06-29 14:57:35 +0200

    add type annotations to Counter example

--- src/Counter.elm
+++ src/Counter.elm
@@ -5,21 +5,28 @@ import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox { init = init, update = update, view = view }
+
+type alias Model = Int
+
+init : Model
+init = 0
 
 type Msg = Increment | Decrement
 
+update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 2
+      model + 1
 
     Decrement ->
       model - 1
 
+view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text "Counter value right now is = ", text (String.fromInt model) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
